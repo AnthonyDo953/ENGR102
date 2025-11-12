dde9e0694d302d54d8a9838da94108531bd2d0e5
Anthony Do <anthonydo953@gmail.com>
2025-11-11 21:22:06 -0600

    Ch11 team labs finished

--- Ch11/passport_checker2.py
+++ Ch11/passport_checker2.py
@@ -7,12 +7,63 @@
 # Anthony Do
 # Will Gipson
 # Section: 217
-# Assignment: 11.9 Passport Checker A
+# Assignment: 11.10 Passport Checker B
 # Date: 10 11 2025
+
+# Take a field on a passport and its associated value and then check if it is valid
+# Return True if it is and False otherwise
+def validField(field, value):
+    match field:
+        case "iyr":
+            if len(value) == 4 and 2015 <= int(value) <= 2025:
+                return True
+            else:
+                return False
+        case "eyr":
+            if len(value) == 4 and 2025 <= int(value) <= 2035:
+                return True
+            else:
+                return False
+        case "hgt":
+            unit = value[-2:]
+            if unit == "cm" and 150 <= int(value.replace("cm", "")) <= 193:
+                return True
+            elif unit == "in" and 59 <= int(value.replace("in", "")) <= 76:
+                return True
+            else:
+                return False
+        case "hcl":
+            if value[0] == "#" and len(value) == 7:
+                for char in value:
+                    if char not in "#1234567890abcdef":
+                        return False
+                return True
+            return False
+        case "ecl":
+            validEcl = ["amb", "blu","brn", "gry", "grn", "hzl", "oth"]
+            if value in validEcl:
+                return True
+            else: 
+                return False
+        case "pid":
+            if len(value) == 9:
+                for char in value:
+                    if char not in "0123456789":
+                        return False
+                return True
+        case "cid":
+            value = value.lstrip("0")
+            if len(value) == 3:
+                return True
+            else:
+                return False
+        case _:
+            return False
+
 
 # Get file name and make file to write to
 file = input("Enter the name of the file: ")
-outfile = open("valid_passports.txt", 'w')
+outfile = open("valid_passports2.txt", 'w')
 
 # Open and read input file
 with open(file, 'r') as scannedPassports:
@@ -50,10 +101,14 @@ with open(file, 'r') as scannedPassports:
             for item in line:
                 passport.update({item[0]: item[1]})
             
-            # Passport is not valid if it doesn't contain all required fields
+            # Passport is not valid if it doesn't contain all required fields or the value does not follow the rules
             for field in requiredFields:
                 if field not in passport.keys():
                     valid = False
+                    break
+                elif not validField(field, passport[field]):
+                    valid = False
+                    break
 
             # If a valid passport, add 1 to counter and write it 
             # Since this the last line track writing to not add an extra empty line at the end of the file
@@ -94,6 +149,11 @@ with open(file, 'r') as scannedPassports:
                 for field in requiredFields:
                     if field not in passport.keys():
                         valid = False
+                        break
+                    elif not validField(field, passport[field]):
+                        valid = False
+                        break
+
 
                 # If a valid passport, add 1 to counter and write it 
                 if valid:
@@ -111,8 +171,3 @@ with open(file, 'r') as scannedPassports:
 outfile.close()
 
 print(f"There are {validPassports} valid passports")
-
-        
-                
-
-
